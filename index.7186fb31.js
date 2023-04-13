fcbdacd33712d88532302b0ad6f8f8be6742c4c2
gulen13 <114436484+gulen13@users.noreply.github.com>
2023-04-13 07:46:23 +0000

    Deploying to gh-pages from @ gulen13/JS-project-02-BOOK@5393f953257d77c2867699856e0f3bdbf56f0c8c 🚀

---
+++ index.7186fb31.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.7186fb31.js.map
---
+++ index.c913b5db.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.c913b5db.js.map
