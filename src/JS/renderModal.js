b55a11741f6f67d1f011b03971b1cc2dd9a8b084
Iryna Zaliska <zaliska@gmail.com>
2023-04-15 14:58:43 +0300

    закривається на кнопку і esc

--- src/JS/renderModal.js
+++ src/JS/renderModal.js
@@ -38,11 +38,11 @@ export async function renderModal(bookID) {
   const markup = `
 
       <div class="modal-book">
-        <img class="modal-book__img" src="${book_image}" alt="Book cover" loading="lazy"/>
+        <img class="modal-book__img" src="${book_image ? book_image : './images/blank-M.jpg'}" alt="Book cover" loading="lazy"/>
         <div class="modal-book__description">
           <div class="modal-book__info">
-            <h2 class="modal-book__title">${title}</h2>
-            <h3 class="modal-book__author">${author}</h3>
+            <h2 class="modal-book__title">${title ? title : 'N/A'}</h2>
+            <h3 class="modal-book__author">${author ? author : 'N/A'}</h3>
             <p class="modal-book__about">${
               description ? description : 'N/A'
             }</p>
@@ -95,22 +95,22 @@ function addToShoppingList(book) {
 
   bookArray.push(oneBook);
   saveToLocalStorage('bookarray', bookArray);
-  isBookInShoppingList = true;
+  // isBookInShoppingList = true;
   // updateModalBtn();
 }
 
 function showModal() {
   modalEl.classList.remove('is-hidden');
   document.addEventListener('keydown', handleCloseModal);
-  // closeModalBtnEl.addEventListener('click', closeModal);
-  backdropEl.addEventListener('click', closeModal);
+  closeModalBtnEl.addEventListener('click', closeModal);
+  // backdropEl.addEventListener('click', closeModal);
 }
 
 function closeModal() {
   modalEl.classList.add('is-hidden');
-  // addBookBtnEl.removeEventListener('keydown', handleCloseModal);
-  // closeModalBtnEl.removeEventListener('click', closeModal);
-  backdropEl.removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', handleCloseModal);
+  closeModalBtnEl.removeEventListener('click', closeModal);
+  // backdropEl.removeEventListener('click', closeModal);
   backdropEl.innerHTML = '';
 }
 
@@ -122,69 +122,6 @@ function handleCloseModal(event) {
     closeModal();
   }
 }
-
-// Данило
-
-// addBookBtnEl.addEventListener('click', addToShoppingList);
-
-// function addToShoppingList() {
-//   const oneBook = { ...book };
-
-//   const array = [];
-// const oneBook = { book_image, title, author, description, buy_links, _id, list_name };
-// Отримуємо з LocalStorage масив книжок (якщо він є)
-// const bookArray = JSON.parse(localStorage.getItem('bookarray')) || [];
-// bookArray.push(oneBook);
-// saveToLocalStorage('bookarray', array);
-// updateModalBtn();
-// }
-
-// Варіант 2
-
-// function addToShoppingList() {
-//   const oneBook = { ...book };
-//   // Отримуємо з LocalStorage масив книжок (якщо він є)
-//   const bookArray = JSON.parse(localStorage.getItem('bookarray')) || [];
-//   bookArray.push(oneBook);
-//   saveToLocalStorage('bookarray', bookArray);
-//   isBookInShoppingList = true;
-//   updateModalBtn();
-// }
-
-// // Варіант 2
-
-// // function addToShoppingList() {
-// //   const oneBook = { ...book };
-// //   const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
-// //   shoppingList.push(oneBook);
-// //   localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-// //   isBookInShoppingList = true;
-// //   updateModalBtn();
-// // }
-
-// // Варіант 3
-
-// // async function addToShoppingList() {
-// //   const oneBook = { ...book };
-// //   const bookArray = await getItemFromLocalStorage('bookarray') || [];
-// //   bookArray.push(oneBook);
-// //   await saveToLocalStorage('bookarray', bookArray);
-// //   isBookInShoppingList = true;
-// //   updateModalBtn();
-// //   }
-
-// function updateModalBtn() {
-//   if (isBookInShoppingList) {
-//     addBookBtnEl.textContent = 'Remove from the shopping list';
-//     const underBtnText = document.createElement('p');
-//     underBtnText.textContent =
-//       'Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".';
-//     underBtnText.classList.add('modal-book__underbtn');
-//     modalEl.appendChild(underBtnText);
-//   } else {
-//     addBookBtnEl.textContent = 'Add to shopping list';
-//   }
-// }
 
 // let isBookInShoppingList = false;
 
@@ -202,6 +139,3 @@ function handleCloseModal(event) {
 //     addBookBtnEl.textContent = 'Add to shopping list';
 //   }
 // }
-
-// src="${book_image ? book_image : './images/blank-M.jpg'}"/>
-// ${title ? title : 'N/A'}
