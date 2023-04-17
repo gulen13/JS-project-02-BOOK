b121ec3ce242ce92830e22d981e0fb41077d17e7
FYuriyF <jufedor@gmail.com>
2023-04-17 16:48:09 +0300

    Work on the design of funds + dynamic numbering

--- src/JS/foundationsItems.js
+++ src/JS/foundationsItems.js
@@ -1,9 +1,9 @@
 import saveTheChildren from '../images/funds/save-the-children.png';
-import saveTheChildren2x from '../images/funds/save-the-children@2x.png';
+import saveTheChildren2 from '../images/funds/save-the-children@2x.png';
 import projectHope from '../images/funds/project-hope.png';
 import projectHope2 from '../images/funds/project-hope@2x.png';
 import united24 from '../images/funds/united24.png';
-import united24х2 from '../images/funds/united24@2x.png';
+import united242 from '../images/funds/united24@2x.png';
 import internationalMedical from '../images/funds/international-medical-corps.png';
 import internationalMedical2 from '../images/funds/international-medical-corps@2x.png';
 import razom from '../images/funds/razom.png';
@@ -19,63 +19,54 @@ import worldVision2 from '../images/funds/world-vision@2x.png';
 
 export const foundationsItems = [
   {
-    number: '01',
     picture1x: saveTheChildren,
-    picture2x: saveTheChildren2x,
+    picture2x: saveTheChildren2,
     link: 'https://www.savethechildren.net/',
     description: 'Save the Children',
   },
   {
-    number: '02',
     picture1x: projectHope,
     picture2x: projectHope2,
     link: 'https://www.projecthope.org/',
     description: 'Project Hope',
   },
   {
-    number: '03',
     picture1x: internationalMedical,
     picture2x: internationalMedical2,
     link: 'https://internationalmedicalcorps.org/',
     description: 'International Medical Corps',
   },
   {
-    number: '04',
     picture1x: razom,
     picture2x: razom2,
     link: 'https://www.razomforukraine.org/',
     description: 'Razom',
   },
   {
-    number: '05',
     picture1x: actionAgainst,
     picture2x: actionAgainst2,
     link: 'https://www.actionagainsthunger.org/',
     description: 'Action Against Hunger',
   },
   {
-    number: '06',
     picture1x: sergiyPrytula,
     picture2x: sergiyPrytula2,
     link: 'https://prytulafoundation.org/en',
     description: 'Serhiy Prytula Charity Foundation',
   },
   {
-    number: '07',
     picture1x: united24,
-    picture2x: united24х2,
+    picture2x: united242,
     link: 'https://u24.gov.ua/uk',
     description: 'UNITED 24',
   },
   {
-    number: '08',
     picture1x: medicinsSans,
     picture2x: medicinsSans2,
     link: 'https://www.msf.org/ukraine',
     description: 'Medicins Sans Frontieres',
   },
   {
-    number: '09',
     picture1x: worldVision,
     picture2x: worldVision2,
     link: 'https://www.wvi.org/emergencies/ukraine',
