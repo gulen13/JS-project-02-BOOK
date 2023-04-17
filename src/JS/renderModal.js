d2c8394cd69c7927f38c8ea09638d1793cd87528
Iryna Zaliska <zaliska@gmail.com>
2023-04-17 16:41:51 +0300

    заглушки на модалку

--- src/JS/renderModal.js
+++ src/JS/renderModal.js
@@ -36,10 +36,24 @@ export async function renderModal(bookID) {
   bookData = book.data;
 
   const markup = `
+    <div class="modal-book">  
+    <picture>
+    <source media="(max-width: 767px)"
+      srcset="${book_image 
+        ? book_image 
+        : './images/blank-l.jpg 1x, ./images/blank-l@2x.jpg 2x'}"
+      type="image/jpg" /> 
 
-      <div class="modal-book">
-        <img class="modal-book__img" src="${book_image ? book_image : './images/blank-M.jpg'
-    }" alt="Book cover" loading="lazy"/>
+      <source media="(min-width: 768px)"
+        srcset="${book_image 
+          ? book_image 
+          : './images/blank-M.jpg 1x, ./images/blank-M@2x.jpg 2x'}"
+        type="image/jpg" /> 
+
+        <img class="modal-book__img" src="${book_image ? book_image : './images/blank-M.jpg'}        
+      }" alt="Book cover" loading="lazy"/>
+    </picture>
+
         <div class="modal-book__description">
           <div class="modal-book__info">
             <h2 class="modal-book__title">${title ? title : 'N/A'}</h2>
@@ -51,24 +65,30 @@ export async function renderModal(bookID) {
             <ul class="book-stores">
               <li class="book-stores__item">
                 <a class="book-stores__link" href="${buy_links.find(link => link.name === 'Amazon').url
+                ? book.buy_links.find(link => link.name === 'Amazon').url
+                : 'https://www.amazon.com/ref=nav_logo'
     }" target="_blank" rel="noopener noreferrer"
                   aria-label="Amazon icon">
-                  <img class="book-stores__img" srcset=" ${amazon} 1x, ${amazon2x}   2x
+                  <img class="book-stores__img" srcset=" ${amazon} 1x, ${amazon2x} 2x
                  "src="${amazon}" alt="Amazon" width="62" height="19">
                   </a>
               </li>
               <li class="book-stores__item">
                 <a class="book-stores__link" href="${buy_links.find(link => link.name === 'Apple Books').url
+                ? book.buy_links.find(link => link.name === 'Apple Books').url
+                : 'https://www.apple.com/ua/apple-books/'
     }" target="_blank" rel="noopener noreferrer"
                   aria-label="Apple Books icon">
-                  <img class="book-stores__img" srcset=" ${ibook} 1x, ${ibook2x}   2x
+                  <img class="book-stores__img" srcset=" ${ibook} 1x, ${ibook2x} 2x
                  "src="${ibook}" alt="Apple Books" width="33" height="32"></a>
               </li>
               <li class="book-stores__item">
                 <a class="book-stores__link" href="${buy_links.find(link => link.name === 'Bookshop').url
+                ? book.buy_links.find(link => link.name === 'Bookshop').url
+                : 'https://www.bookshop.org/books/'
     }" target="_blank" rel="noopener noreferrer"
                   aria-label="Bookshop icon">
-                  <img class="book-stores__img" srcset=" ${bookshop} 1x, ${bookshop2x}   2x
+                  <img class="book-stores__img" srcset=" ${bookshop} 1x, ${bookshop2x} 2x
                  "src="${bookshop}" alt="Bookshops" width="38" height="36"></a>
               </li>
             </ul>   
