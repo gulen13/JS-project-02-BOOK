66d5892640a554daa606247d0d4a00d9c58d5210
Danil Deineka <tripledpadik@gmail.com>
2023-04-18 15:59:37 +0300

    add functional for see more button on current page + change fav icon

--- src/JS/booksListGenerator.js
+++ src/JS/booksListGenerator.js
@@ -1,7 +1,7 @@
 import { BooksAPI } from './fetchBooksAPI';
 
 const mainSectionDiv = document.querySelector('.books-best-container');
-const booksTitileHome = document.querySelector('.books-section__title'); 
+const booksTitileHome = document.querySelector('.books-section__title');
 
 window.addEventListener('resize', function (event) {
   mainSectionDiv.innerHTML = '';
@@ -11,7 +11,7 @@ window.addEventListener('resize', function (event) {
 
 export async function getTopBooks() {
   booksTitileHome.classList.remove('display-none');
-  mainSectionDiv.innerHTML="";
+  mainSectionDiv.innerHTML = '';
   try {
     const booksAPI = new BooksAPI();
     const booksCategories = await booksAPI.fetchTopBooks();
@@ -71,7 +71,7 @@ function createBlock(categoryList) {
 function createCard(books, cardsAmount, cardsBlock) {
   for (let j = 0; j < cardsAmount; j++) {
     const blockCard = `
-      
+
         <li class="list-book__book-card"  data-id="${books[j]._id}">
           <a href="#">
           <div class="card-thumb">
@@ -80,9 +80,9 @@ function createCard(books, cardsAmount, cardsBlock) {
             </div>
             <h2 class="card__title">${books[j].title}</h2>
             <p class="card__paragraph">${books[j].author}</p>
-          </a>  
-        </li> 
-      
+          </a>
+        </li>
+
     `;
 
     cardsBlock.innerHTML += blockCard;
@@ -97,12 +97,10 @@ export async function createMarkupCategory(category) {
   // console.log(selectedCategoryBooks.data);
 
   const cardsBlock = `
-      
       <div class="block-for-books">
       <h2 class="block-for-books__title">${category}</h2>
       <ul class="block-for-books__list"></ul>
       </div>
-      
     `;
   mainSectionDiv.innerHTML = cardsBlock;
 
@@ -112,7 +110,6 @@ export async function createMarkupCategory(category) {
 
   selectedCategoryBooks.data.forEach(book => {
     const blockCard = `
-      
         <li class="list-book__book-card"  data-id="${book._id}">
           <a href="#">
           <div class="card-thumb">
@@ -121,9 +118,8 @@ export async function createMarkupCategory(category) {
             </div>
             <h2 class="card__title">${book.title}</h2>
             <p class="card__paragraph">${book.author}</p>
-          </a>  
-        </li> 
-      
+          </a>
+        </li>
     `;
 
     cardsContainer.innerHTML += blockCard;
@@ -131,3 +127,84 @@ export async function createMarkupCategory(category) {
 }
 
 getTopBooks();
+
+
+
+
+// замінити функцію для відображення see more!!!!!!!!!!!!!!!!!!!!!
+
+
+// function createBlock(categoryList) {
+//   const blockForCard = `
+//     <div class="books-section__list-markup">
+//       <p class="list-markup__paragraph">${categoryList.list_name}</p>
+//       <ul class="list-markup__block" data-ulll="${categoryList.list_name}"></ul>
+//       <div class="list-markup__button-wrapper">
+//         <button class="list-markup__see-more-button" data-category="${categoryList.list_name}">see more</button>
+//       </div>
+//     </div>
+//   `;
+
+
+
+//   mainSectionDiv.innerHTML += blockForCard;
+
+//   const cardsContainer = mainSectionDiv.lastElementChild.querySelector(
+//     '.list-markup__block'
+//   );
+
+//   const seeMoreButtons = mainSectionDiv.querySelectorAll(
+//     '.list-markup__see-more-button'
+//   );
+//   seeMoreButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//       const category = button.dataset.category;
+//       createMarkupCategory(category);
+//       button.classList.toggle('hide');
+//       button.innerHTML = 'Hide';
+//       if (button.classList.contains("hide")) {
+//         console.log('hide');
+//         button.addEventListener('click', getTopBooks)
+//       }
+//     });
+//   })
+
+//   adaptiveMarkup(categoryList.books, cardsContainer);
+  
+// }
+
+
+// export async function createMarkupCategory(category) {
+//   booksTitileHome.classList.add('display-none');
+//   const booksAPI = new BooksAPI();
+//   const selectedCategoryBooks = await booksAPI.fetchBooksByCategory(category);
+
+
+//   const ul = document.querySelector(`[data-ulll="${category}"]`);
+
+//   console.log(ul);
+
+//   ul.innerHTML = '';
+
+//   selectedCategoryBooks.data.forEach(book => {
+//     const blockCard = `
+      
+//         <li class="list-book__book-card"  data-id="${book._id}">
+//           <a href="#">
+//           <div class="card-thumb">
+//             <img src="${book.book_image}" alt="${book.title}" class="book-card__img"></img>
+//             <p class="card-overlay-text card-overlay">quick view</p>
+//             </div>
+//             <h2 class="card__title">${book.title}</h2>
+//             <p class="card__paragraph">${book.author}</p>
+//           </a>  
+//         </li> 
+      
+//     `;
+
+//     ul.innerHTML += blockCard;
+//   });
+// }
+
+// getTopBooks();
+
