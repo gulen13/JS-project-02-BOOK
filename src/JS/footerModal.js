e9fd97559e9ea2775a9980c502f6ab6c581805ee
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-04-20 09:56:43 +0300

    add changes
    photo
    js

--- src/JS/footerModal.js
+++ src/JS/footerModal.js
@@ -1,14 +1,37 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-footer]'),
-    closeModalBtn: document.querySelector('[data-modal-close-footer]'),
-    modal: document.querySelector('[data-modal-footer]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+ const refs = {
+   openModalBtn: document.querySelector('[data-modal-open-footer]'),
+   closeModalBtn: document.querySelector('[data-modal-close-footer]'),
+   modal: document.querySelector('[data-modal-footer]'),
+};
+refs.openModalBtn.addEventListener('click', handleOpenModal);
+ 
+function addEvtListener() {
+  refs.modal.addEventListener('click', handleCloseModal);
+  refs.closeModalBtn.addEventListener('click', handleCloseModal);
+  document.addEventListener('keydown', handleEscCloseFooterModal);
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+function removeEvtListener() {
+  refs.modal.addEventListener('click', handleCloseModal);
+  refs.closeModalBtn.removeEventListener('click', handleCloseModal);
+  document.removeEventListener('keydown', handleEscCloseFooterModal);
+}
+
+function handleOpenModal() {
+  refs.modal.classList.remove('is-hidden');
+  addEvtListener();
+}
+
+function handleCloseModal() {
+   refs.modal.classList.add('is-hidden');
+   removeEvtListener();
+}
+
+function handleEscCloseFooterModal(event) {
+
+  if (event.type === 'keydown' && event.key === 'Escape') {
+    handleCloseModal();
   }
-})();
+
+}
